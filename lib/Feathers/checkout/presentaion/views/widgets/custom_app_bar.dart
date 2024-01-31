a7a04ac1d030d30fa838845bde4140c8b4f3c65f
Mazen Gamal <mazeng18@gmail.com>
2024-01-31 12:10:33 +0200

    create paymentIntentModel & create payment_intent_input_model.dart & create postApiService

--- lib/Feathers/checkout/presentaion/views/widgets/custom_app_bar.dart
+++ lib/Feathers/checkout/presentaion/views/widgets/custom_app_bar.dart
@@ -1,11 +1,14 @@
 import 'package:checkout_app/core/utils/styles.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_svg/svg.dart';
 
 AppBar customAppBar({ final String? title, void Function()? onTapBack}) {
   return AppBar(
-
     backgroundColor: Colors.transparent,
+    systemOverlayStyle: const SystemUiOverlayStyle(
+     statusBarIconBrightness: Brightness.dark,
+    ),
     centerTitle: true,
     leading: Center(
         child: GestureDetector(
---
+++ lib/core/utils/stripe_service.dart
@@ -0,0 +1,11 @@
+import 'package:checkout_app/Feathers/checkout/data/models/payment_intent_input_model.dart';
+import 'package:checkout_app/Feathers/checkout/data/models/payment_intent_model/Payment_intent_model.dart';
+
+class StripeService
+{
+
+  // PaymentIntentModel createPaymentIntent(PaymentIntentInputModel paymentIntentInputModel)
+  // {
+  //
+  // }
+}
